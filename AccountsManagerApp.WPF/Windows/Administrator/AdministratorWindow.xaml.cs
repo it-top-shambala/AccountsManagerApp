2b9cddba5dc4f814df53779494fa4d473608c17d
Starinin Andrey <starininandrey@gmail.com>
2023-11-11 16:38:37 +0300

    Создание окна администратора

---
+++ AccountsManagerApp.WPF/Windows/Administrator/AdministratorWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace AccountsManagerApp.WPF.Windows.Administrator;
+
+public partial class AdministratorWindow : Window
+{
+    public AdministratorWindow()
+    {
+        InitializeComponent();
+    }
+}
